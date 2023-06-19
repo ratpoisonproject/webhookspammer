855801f6e2178cbc9d5359af42b3c9d6b8f86c61
electron271 <aveanzaldua@gmail.com>
2023-06-18 20:37:48 -0500

    finish spam command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{Read, stdout, Write};
-
+use dialoguer::{Input, Password, Confirm, theme::ColorfulTheme};
 use crossterm::style::Stylize;
 use serde_json::json;
 
@@ -43,7 +43,7 @@ fn main() {
                 println!("{}", "──────────────────".grey());
                 println!("{}", "help - show this message".italic().grey());
                 println!("{}", "exit - exit the program".italic().grey());
-                println!("{}", "spam <url> - spam a webhook".italic().grey());
+                println!("{}", "spam - spam a webhook".italic().grey());
                 println!("{}", "──────────────────".grey());
             },
             "exit" => {
@@ -51,55 +51,101 @@ fn main() {
                 break;
             },
             "spam" => {
-                // if not enough arguments
-                if args.len() < 2 {
-                    println!("{}", "not enough arguments".italic().grey());
-                    continue;
-                }
+                // ask for webhook url
+                let webhook_url = Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Enter your Discord webhook URL")
+                    .validate_with({
+                        // check if the url is valid
+                        // send a GET request to the url and check if the response is 200
+                        |input: &String| -> Result<(), &str> {
+                            let client = reqwest::blocking::Client::new();
+                            let res = client.get(input).send();
+                            match res {
+                                Ok(res) => {
+                                    if res.status() == 200 {
+                                        Ok(())
+                                    } else {
+                                        Err("Invalid webhook URL")
+                                    }
+                                }
+                                Err(_) => Err("Invalid webhook URL"),
+                            }
+                        }
+                    })
+                    .interact_text()
+                    .unwrap();
 
-                // send "test" 5 times
-                let url = args[1];
-                let client = reqwest::blocking::Client::new();
-                // check if url is valid
-                if !url.contains("https://discord.com/api/webhooks") {
-                    println!("{}", "invalid url".italic().grey());
-                    continue;
-                }
+                // ask for message
+                let message: String = Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Enter the message you want to spam")
+                    .interact_text()
+                    .unwrap();
 
-                // check if the webhook is valid
-                let res = client.get(url).send();
-                if res.is_err() {
-                    println!("{}", "invalid webhook".italic().grey());
-                    continue;
-                }
+                // ask for amount
+                let amount: String = Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Enter the amount of times you want to spam the webhook")
+                    .validate_with({
+                        // check if the amount is valid
+                        // make sure it's a number
+                        |input: &String| -> Result<(), &str> {
+                            match input.parse::<u32>() {
+                                Ok(_) => Ok(()),
+                                Err(_) => Err("Invalid amount"),
+                            }
+                        }
+                    })
+                    .interact_text()
+                    .unwrap();
 
-                // send the message
-                for _ in 0..5 {
-                    let res = client.post(url).json(&json!({
-                        "content": "test"
-                    })).send();
-                    if res.is_err() {
-                        println!("{}", "invalid webhook".italic().grey());
-                        continue;
+                // convert amount to u32
+                let amount: u32 = amount.parse().unwrap();
+
+                // ask for username
+                let username: String = Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Enter the username you want to use")
+                    .interact_text()
+                    .unwrap();
+
+                // convert all the strings to &str
+                let webhook_url: &str = &webhook_url;
+                let message: &str = &message;
+                let username: &str = &username;
+    
+
+                println!("{}", "──────────────────".grey());
+                println!("{}", message.italic().grey());
+                println!("{}", webhook_url.italic().grey());
+                println!("{}", amount.to_string().italic().grey());
+                println!("{}", username.italic().grey());
+                println!("{}", "──────────────────".grey());
+
+                // ask for confirmation
+                let confirm = Confirm::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Are you sure you want to spam this webhook?")
+                    .interact()
+                    .unwrap();
+                
+                if confirm {
+                    // send the requests
+                    let client = reqwest::blocking::Client::new();
+                    for i in 0..amount {
+                        let res = client.post(&*webhook_url)
+                            .json(&json!({
+                                "content": message,
+                                "username": username
+                            }))
+                            .send();
+                        match res {
+                            Ok(_) => {
+                                println!("{} {} {} {}", "sent message".italic().grey(), i.to_string().bold().green(), "out of".italic().grey(), amount.to_string().bold().green());
+                            }
+                            Err(_) => {
+                                println!("{}", "failed to send message".italic().grey());
+                            }
+                        }
                     }
-                }
-
-                println!("{}", "sent 5 messages".italic().grey());
-
-                // ask if they want to delete the webhook
-                print!("{} ", "delete webhook? (y/n)".yellow());
-                stdout().flush().unwrap();
-                let mut input = String::new();
-                std::io::stdin().read_line(&mut input).unwrap();
-                input.pop(); // remove newline
-
-                if input == "y" {
-                    let res = client.delete(url).send();
-                    if res.is_err() {
-                        println!("{}", "invalid webhook".italic().grey());
-                        continue;
-                    }
-                    println!("{}", "deleted webhook".italic().grey());
+                } else {
+                    println!("{}", "cancelled".italic().grey());
                 }
             },
             _ => {
