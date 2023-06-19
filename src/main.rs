24824735b5028c5c246734ffd8ed5e11874171b3
electron271 <aveanzaldua@gmail.com>
2023-06-18 20:13:15 -0500

    add simple test spam

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,80 @@ fn main() {
     println!("{}", "──────────────────".grey());
     
     loop {
-        println!("{}", print_cli_prompt());
+        let mut input = print_cli_prompt();
+        input.pop(); // remove newline
+
+        let args: Vec<&str> = input.split(" ").collect();
+        let command = args[0];
+        match command {
+            "help" => {
+                println!("{}", "──────────────────".grey());
+                println!("{}", "help".bold().green());
+                println!("{}", "──────────────────".grey());
+                println!("{}", "help - show this message".italic().grey());
+                println!("{}", "exit - exit the program".italic().grey());
+                println!("{}", "spam <url> - spam a webhook".italic().grey());
+                println!("{}", "──────────────────".grey());
+            },
+            "exit" => {
+                println!("{}", "exiting...".italic().grey());
+                break;
+            },
+            "spam" => {
+                // if not enough arguments
+                if args.len() < 2 {
+                    println!("{}", "not enough arguments".italic().grey());
+                    continue;
+                }
+
+                // send "test" 5 times
+                let url = args[1];
+                let client = reqwest::blocking::Client::new();
+                // check if url is valid
+                if !url.contains("https://discord.com/api/webhooks") {
+                    println!("{}", "invalid url".italic().grey());
+                    continue;
+                }
+
+                // check if the webhook is valid
+                let res = client.get(url).send();
+                if res.is_err() {
+                    println!("{}", "invalid webhook".italic().grey());
+                    continue;
+                }
+
+                // send the message
+                for _ in 0..5 {
+                    let res = client.post(url).json(&json!({
+                        "content": "test"
+                    })).send();
+                    if res.is_err() {
+                        println!("{}", "invalid webhook".italic().grey());
+                        continue;
+                    }
+                }
+
+                println!("{}", "sent 5 messages".italic().grey());
+
+                // ask if they want to delete the webhook
+                print!("{} ", "delete webhook? (y/n)".italic().grey());
+                stdout().flush().unwrap();
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).unwrap();
+                input.pop(); // remove newline
+
+                if input == "y" {
+                    let res = client.delete(url).send();
+                    if res.is_err() {
+                        println!("{}", "invalid webhook".italic().grey());
+                        continue;
+                    }
+                    println!("{}", "deleted webhook".italic().grey());
+                }
+            },
+            _ => {
+                println!("{}", "invalid command".italic().grey());
+            }
+        }
     }
 }
